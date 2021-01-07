58602f6832ac298dcf394301a19093d1ac1b33ed
Jerin Johny <71542613+jerinjohny-ktnm@users.noreply.github.com>
2021-01-07 08:47:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@
 module.exports = {
 
     //your bot api key get it from botfather
-    bot_api_key: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    bot_api_key: "1507562704:AAENTw2XdeYNheSkUds3ZcoU7t-SZ9WlaSM",
 
     //Jiosaavn api url {url format will be same as following}
     // create your jiosaavn api from https://github.com/cachecleanerjeet/jiosaavnapi
@@ -13,15 +13,15 @@ module.exports = {
 
     // replace with your own musicder download url {url format will be same as following}
     // create your musicder from https://github.com/cachecleanerjeet/Musicder
-    musicder_url: "https://musicder.net/",
+    musicder_url: "https://musicom.ml/",
 
     // Customize your /start message {html is supported}
-    start_message: "<b>Hey, 👋👋</b>\n\n<em>Welcome to <b>Musicder Bot</b>.\n\nJust send the Song's Name or, Jiosaavn Song's Link & You will get Best Matched Result(s) with their Download Link(s).\n\nBrought you by @tprojects</em>",
+    start_message: "<b>Hey, 👋👋</b>\n\n<em>Welcome to <b>MusiCom Bot</b>.\n\nJust send the Song's Name or, Jiosaavn Song's Link & You will get Best Matched Result(s) with their Download Link(s).\n\nBrought you by @imjerin</em>",
 
     // Customize your /about message {html is supported}
-    about_message: "Created by <a href='https://tu.hin.life'>Tuhin Kanti Pal</a>.",
+    about_message: "Created by <a href='https://jerinjohny.ml'>Jerin Johny</a>.",
 
     // Customize your /help message {html is supported}
-    help_message: "Created by <a href='https://tu.hin.life'>Tuhin Kanti Pal</a>.",
+    help_message: "Created by <a href='https://jerinjohny.ml'>Jerin Johny</a>.",
 
 };
